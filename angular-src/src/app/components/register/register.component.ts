940ded4cd9e6f3505a08d450111e11820d867709
Nomad21 <np130@hw.ac.uk>
2017-02-26 23:50:39 +0400

    added register form html and validation.

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from "../../services/validate.service";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  password: String;
+  email: String;
+  username: String;
+
+  constructor(
+    private validateService: ValidateService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+
+// Required fields
+    if(!this.validateService.validateRegister(user)){
+      console.log("pls fill all fields");
+      return false;
+    }
+
+// Validate email
+    if(!this.validateService.validateEmail(user.email)){
+      console.log("pls enter valid email");
+      return false;
+    }
+
   }
 
 }
